72bd6aa6169606b88608de468b17da843c994acc
Chonix <jhonnyx@gmail.com>
2013-12-05 23:56:07 -0300

    uploaded local device branch

---
+++ omni_maguro.mk
@@ -0,0 +1,29 @@
+# Copyright (C) 2013 OmniROM Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Inherit Omni GSM telephony parts
+$(call inherit-product, vendor/omni/config/gsm.mk)
+
+# Inherit from our omni product configuration
+$(call inherit-product, vendor/omni/config/common.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/samsung/maguro/full_maguro.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := omni_maguro
+PRODUCT_DEVICE := maguro
+PRODUCT_BRAND := google
+PRODUCT_MANUFACTURER := Samsung
+PRODUCT_MODEL := Galaxy Nexus
